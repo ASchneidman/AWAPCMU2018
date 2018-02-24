b50f5743f66c8bc3c45899246c8059b64c35dab0
Uday Shankar <us@andrew.cmu.edu>
2018-02-24 15:27:07 -0500

    glass cannon kind of works

--- Algo2018/awap2018-competition/src/glasscannon.py
+++ Algo2018/awap2018-competition/src/glasscannon.py
@@ -11,7 +11,7 @@ class Player(BasePlayer):
 
     def __init__(self, p_id):
         super().__init__(p_id)  #Initializes the super class. Do not modify!
-        self.nucleus = self.nodes[0]
+        self.nucleus = None
         """
         Insert player-specific initialization code here
         """
@@ -23,6 +23,10 @@ class Player(BasePlayer):
     """
     def init_turn(self, board, nodes, max_units):
         super().init_turn(board, nodes, max_units)       #Initializes turn-level state variables
+        if self.nucleus == None:
+            for node in self.nodes:
+                self.nucleus = node
+
         """
         Insert any player-specific turn initialization code here
         """
@@ -39,6 +43,17 @@ class Player(BasePlayer):
         self.place_unit(self.nucleus, self.max_units)
         return self.dict_moves #Returns moves built up over the phase. Do not modify!
 
+    def get_weakest(self):
+        print("hello")
+        weakest=None
+        weakness=99999999999999
+        for neighbor in self.board[self.nucleus]:
+            n = self.board.nodes[neighbor]
+            if(n['old_units'] <= weakness and n['owner'] != self.player_num):
+                weakness = n['old_units']
+                weakest = neighbor
+        return weakest, weakness
+
     """
     Called during the move phase to request player moves
     """
@@ -46,18 +61,9 @@ class Player(BasePlayer):
         """
         Insert player logic here to determine where to move your units
         """
-        target, strength= get_weakest()
-        if(strength-1<self.board.nodes[self.nucleus]['old_units']):
+        target, strength= self.get_weakest()
+        if(target != None and strength-1 < self.board.nodes[self.nucleus]['old_units']):
             self.move_unit(self.nucleus, target, self.board.nodes[self.nucleus]['old_units']-1)
-            self.nucleus=target
+            self.nucleus = target
 
         return self.dict_moves #Returns moves built up over the phase. Do not modify!
-
-    def get_weakest(self):
-        weakest=None
-        weakness=99999999999999
-        for neighbor in self.board[self.nucleus]:
-            if(self.board.nodes[neighbor]['old_units']<=weakness and self.board.nodes[neighbor]['owner']!= self.player_num):
-                weakness=self.board.nodes[neighbor]['old_units']
-                weakest=self.board.nodes[neighbor]
-        return weakest, weakness
